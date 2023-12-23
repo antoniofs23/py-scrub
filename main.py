52b2509f8e05a9ab73cacb769f023af594affcfa
antoniofs23 <af3036@nyu.edu>
2023-12-23 13:42:11 -0500

    added dict with common file extensions

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+
+# change the working directory when script is run through command-line
+abspath = os.path.abspath(__file__)
+dirname = os.path.dirname(abspath)
+os.chdir(dirname)
+
+dir_dict = {'Documents': ['.DOC', '.DOCX', '.HTML', '.HTM', '.ODT',
+                          '.PDF', '.XLS', '.XLSX', '.ODS', '.PPT', '.PPTX', '.TXT', '.MD'],
+            'Pictures': ['.JPEG', '.JPG', '.GIF', '.TIFF', '.PSD', '.EPS', '.AI', '.INDD',
+                         '.RAW', '.PNG', '.BMP', '.XCF'],
+            'Video': ['.MP4', '.MOV', '.WMV', '.AVI', '.AVCHD', '.FLV', '.F4V', '.SWF',
+                      '.MKV', '.WEBM', '.HTML5'],
+            'Music': ['.M4A', '.FLAC', '.MP3', '.WAV', '.WMA', '.AAC']}
+
+common_str = '/home/'+os.getlogin()+'/'
+
+# determine the file extension
+
+
+def get_extension(filename):
+
+    split_filename = os.path.splitext(filename)
+
+    return split_filename[1]
+
+
+# get files in working directory
+files = os.listdir()
+for file in files:
+
+    extension = get_extension(file)
+    file_name = dirname+'/'+file
+
+    if extension in dir_dict['Documents']:
+        file_str = common_str+'Documents/'+file
+    elif extension in dir_dict['Pictures']:
+        file_str = common_str+'Pictures/'+file
+    elif extension in dir_dict['Video']:
+        file_str = common_str+'Video/'+file
+    elif extension in dir_dict['Music']:
+        file_str = common_str+'Music/'+file
+
+    os.replace(file_name, file_str)
